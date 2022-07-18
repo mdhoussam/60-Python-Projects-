5b5bd3121736637b98e75034857d09102d16db6d
mdhoussam <104451192+mdhoussam@users.noreply.github.com>
2022-07-18 12:01:03 +0600

    nice

--- Beginners/8.QR Code Generator.py
+++ Beginners/8.QR Code Generator.py
@@ -1,11 +1,14 @@
-import pyqrcode
-from pyqrcode import QRCode
 
-# String which represent the QR code
-s = "https://www.youtube.com/channel/UCeO9hPCfRzqb2yTuAn713Mg"
-
-# Generate QR code
-url = pyqrcode.create(s)
-
-# Create and save the png file naming "myqr.png"
-url.svg("myyoutube.svg", scale=8)
+import qrcode
+#qr = qrcode.make('helle etete')
+#qr.save('myQR.png')
+qr = qrcode.QRCode(
+   version= 1,
+    box_size=15,
+    border=5
+)
+data = 'https://github.com/mdhoussam'
+qr.add_data(data)
+qr.make(fit=True)
+img = qr.make_image(fill ='black',black_color='white')
+img.save('Github Channel QR Code.png')
