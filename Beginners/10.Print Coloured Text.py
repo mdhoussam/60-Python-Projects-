4578a126de97bacdeed3bb636c0f45889e6a7321
mdhoussam <104451192+mdhoussam@users.noreply.github.com>
2022-07-21 04:00:54 +0600

    nice

--- Beginners/10.Print Coloured Text.py
+++ Beginners/10.Print Coloured Text.py
@@ -1,26 +1,7 @@
-def check_guess(guess, answer):
-    global score
-    still_guessing = True
-    attempt = 0
-    while still_guessing and attempt < 3:
-        if guess.lower() == answer.lower():
-            print("Correct Answer")
-            score = score + 1
-            still_guessing = False
-        else:
-            if attempt < 2:
-                guess = input("Sorry Wrong Answer, try again")
-            attempt = attempt + 1
-    if attempt == 3:
-        print("The Correct answer is ", answer)
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
 
-
-score = 0
-print("Guess the Animal")
-guess1 = input("Which bear lives at the North Pole? ")
-check_guess(guess1, "polar bear")
-guess2 = input("Which is the fastest land animal? ")
-check_guess(guess2, "Cheetah")
-guess3 = input("Which is the larget animal? ")
-check_guess(guess3, "Blue Whale")
-print("Your Score is " + str(score))
+print(Fore.BLUE+Back.RED+"Hi My name is Mohamed Islam "+ Fore.YELLOW+ Back.BLUE+"I am your Machine Learning Instructor")
+print(Back.CYAN+"Hi My name is Houssam"+ Fore.BLUE+ Back.RED+ "houssam  from comoros")
+print(Fore.RED + Back.GREEN+ "Hi My name is etetet")
